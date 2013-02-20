71081a7f5ee81ac19c25153d8cb1b10884906a74
vxf <vasco.flores@gmail.com>
2013-02-20 23:24:34 +0000

    clean

--- euler31.erl
+++ euler31.erl
@@ -1,16 +1,13 @@
 -module(euler31).
 -author("vxf").
 -export([start/0]).
--import(file, [read_line/1, open/2]).
--import(string, [tokens/2]).
--import(lists, [sum/1]).
 
 coins(200, _, _) ->
 	1;
 coins(A, _, _) when A > 200 ->
 	0;
 coins(A, L, C) when A < 200 ->
-	sum([coins(A + X, L, X) ||
+	lists:sum([coins(A + X, L, X) ||
 		X <- L, X =< C]).
 
 coins() -> coins(0, [200,100,50,20,10,5,2,1], 200).
