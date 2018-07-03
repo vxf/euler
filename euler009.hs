1a15b59237a4c4e07ce42518bbf6b75c1301226c
vxf <vasco.flores@gmail.com>
2018-07-03 04:52:50 +0100

    new solution for 9 in functional programming

--- euler009.hs
+++ euler009.hs
@@ -1,4 +1,4 @@
-"""
+{-
 proposition:
 a, b, c natural numbers (positive integers)
 a + b + c = 1000
@@ -13,20 +13,14 @@ b*(-2000 + 2*a) = -1000^2 + 2000*a
 
 b = (-1000^2 + 2000*a) / (-2000 + 2*a) # and still b must be a positive and integer
 
-we don't need to test all 'a' until 1000, just until conditions are met
-"""
+we need just to test 'a' until conditions are met
+-}
 
-def fb(a):
-    q, r = divmod(-1000**2 + 2000*a, -2000 + 2*a)
-    
-    return a, q, r
+findABC = go [1..] where
+  go (a : as) =
+    let (b, r) = divMod (-1000^2 + 2000*a) (-2000 + 2*a) in
+      if r /= 0 || b <= 0 then go as
+      else a * b * (1000 - a - b)
 
-a, b = next((a, b)
-    for a, b, r
-    in map(fb, range(1,1000))
-    if r == 0 and b > 0)
-
-c = 1000 - a - b
-
-print(a*b*c)
-
+main = do
+    print(findABC)
